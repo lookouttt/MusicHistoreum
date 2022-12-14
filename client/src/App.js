09de61decb412a6e51fdce0f50214e7219809732
lookouttt <lookoutt@hotmail.com>
2022-12-14 00:20:44 -0800

    Temporary test to verify React can retrieve the DB data.  It worked!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import './App.css';
 
 function App() {
+    useEffect(() => {
+        fetch("http://localhost:5000/chart/1/Song/Week/1989-11-11")
+        .then((response) => response.json())
+        .then((actualData) => console.log(actualData))
+        .catch((err) => {
+            console.log(err.message);
+        });
+    }, []);
     return (
         <div className="App">
             <Header />
