2371d77ec37b2cc958b2e898a88737acb1424ce0
lookouttt <lookoutt@hotmail.com>
2022-12-23 15:14:28 -0800

    Handle DB Credentials

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "1202ThurnRidge",
-    host: "localhost",
-    port: 5432,
-    database: "BillboardData"
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
 });
 
 module.exports = pool;
