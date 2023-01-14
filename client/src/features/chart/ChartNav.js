0ed66c94290c68ad598176f71301f3c5a7b10cab
lookouttt <lookoutt@hotmail.com>
2023-01-13 17:28:39 -0800

    Got Prev and Next buttons working.

--- client/src/features/chart/ChartNav.js
+++ client/src/features/chart/ChartNav.js
@@ -1,28 +1,51 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import { Button, Container, Row, Col } from "reactstrap";
 import { selectSpecificChart } from "../chartMenu/chartsMenusSlice";
 import WeekPicker from '../../utils/WeekPicker';
 import MonthPicker from '../../utils/MonthPicker';
 import YearPicker from '../../utils/YearPicker';
 import DecadePicker from '../../utils/DecadePicker';
+import { updateCurrentChart, updatePendingDate } from '../chart/chartsSlice';
+const dayjs = require("dayjs");
 
 const ChartNav = ({chart}) => {
-    const { chartType, chartId, chartTimeframe } = chart;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { chartType, chartId, chartTimeframe, chartDate } = chart;
     const thisChart = useSelector(selectSpecificChart(chartType, chartId));
+
+    const getNewChart = (chartDate) => {
+         dispatch(updatePendingDate(chartDate));
+         dispatch(updateCurrentChart());
+         navigate('/Chart');
+    }
+
     let picker;
+    let prevDate;
+    let nextDate;
+
     switch (chartTimeframe) {
         case 'Week':
             picker = <><WeekPicker firstDate={thisChart.FirstDate} lastDate={thisChart.LastDate}/></>;
+            nextDate = dayjs(chartDate).add(1,'week');
+            prevDate = dayjs(chartDate).subtract(1,'week');
             break;
         case 'Month':
             picker = <><MonthPicker firstDate={thisChart.FirstDate} lastDate={thisChart.LastDate}/></>;
+            nextDate = dayjs(chartDate).add(1,'month');
+            prevDate = dayjs(chartDate).subtract(1,'month');
             break;
         case 'Year':
             picker = <><YearPicker firstDate={thisChart.FirstDate} lastDate={thisChart.LastDate}/></>;
+            nextDate = dayjs(chartDate).add(1,'year');
+            prevDate = dayjs(chartDate).subtract(1,'year');
             break;
         case 'Decade':
             picker = <><DecadePicker firstDate={thisChart.FirstDate} lastDate={thisChart.LastDate}/></>;
+            nextDate = dayjs(chartDate).add(10,'year');
+            prevDate = dayjs(chartDate).subtract(10,'year');
             break;
         default:
             break;     
@@ -32,13 +55,13 @@ const ChartNav = ({chart}) => {
         <Container className='chartNavCont'>
             <Row className="row-cols-lg-auto g-3">
                 <Col>
-                    <Button color='primary' size='sm'>Prev</Button>
+                    <Button color='primary' size='sm' onClick={() => getNewChart(prevDate)} >Prev</Button>
                 </Col>
                 <Col>
                     {picker}
                 </Col>
                 <Col>
-                    <Button color='primary' size='sm'>Next</Button>
+                    <Button color='primary' size='sm' onClick={() => getNewChart(nextDate)} >Next</Button>
                 </Col>
             </Row>
         </Container>
