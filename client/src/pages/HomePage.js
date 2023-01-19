55466232898a63a92e278943a6c4f79b2c53c160
lookouttt <lookoutt@hotmail.com>
2023-01-18 22:41:13 -0800

    Updated the home page.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Card, CardHeader, CardBody } from "reactstrap";
 import '../App.css';
 
 const HomePage = () => {
@@ -7,16 +7,38 @@ const HomePage = () => {
             <Row className="justify-content-md-center">
                 <Col>
                     <section className='mh-background' data-urltype='HomePage'>
-                        <div>
-                            <h1 id="landing-title">
-                                Welcome to Music Historeum
-                            </h1>
-                            <p id="landing-content">
-                            Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. 
-                            Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. 
-                            Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
-                            </p>
-                        </div>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Card id='home-card'>
+                                        <CardHeader>
+                                            <h1 id="home-title">
+                                                Welcome to Music Historeum!!
+                                            </h1>
+                                        </CardHeader>
+                                        <CardBody>
+                                            <p id="home-content">
+                                                If you enjoy music, then this site is for you. Here you can view music chart data from all around 
+                                                the world of music. Wondering about the current charts? We've got them here. Been curious what the 
+                                                top ten songs were during the week you were born? You can find it here.
+
+                                            <br/>
+                                            <br/>
+                                                This site is still under contruction and being updated regularly. Check out the Future Features section
+                                                to see where we are headed with the site. Feel free to use the contact form to offer any suggestions
+                                                or feedback you may have.
+
+                                            <br/>
+                                            <br/>
+                                                Now, take a look around. Use the Charts menu above to find the chart information you're looking for. we
+                                                have weekly chart data for the Top Songs and Top Albums. We also have monthly, yearly, and decade charts 
+                                                for those two categoaries as well as for other genre-specific categories. We hope you enjoy the site.
+                                            </p>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
                     </section>
                 </Col>
             </Row>
