e6577eefd78c727a0919c19f18e8e830a720f39e
lookouttt <lookoutt@hotmail.com>
2023-02-09 22:32:19 -0800

    Basic filtering on tables is working.  Saving this off before moving to more advanced filtering.

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,9 +1,92 @@
 import React, { useEffect } from "react";
-import { usePagination, useTable } from "react-table";
+import { useFilters, useGlobalFilter, usePagination, useTable, useAsyncDebounce } from "react-table";
 import { useNavigate } from 'react-router-dom';
+import {matchSorter} from 'match-sorter';
+
+// Define a default UI for filtering
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
 
 export default function Table({ columns, data, hiddenColumns = [], onCloseModal, tablePageSize }) {
     let prevHiddenColumns = [];
+      const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
   // Use the useTable Hook to send the columns and data to build the table
   const {
     getTableProps, // table props from react-table
@@ -22,11 +105,17 @@ export default function Table({ columns, data, hiddenColumns = [], onCloseModal,
     previousPage,
     setPageSize,
     state: { pageIndex, pageSize },
+    preGlobalFilteredRows,
+    setGlobalFilter,
   } = useTable({
-    columns,
-    data,
-    initialState: { pageIndex: 0, pageSize: tablePageSize},
+        columns,
+        data,
+        initialState: { pageIndex: 0, pageSize: tablePageSize},
+        defaultColumn,
+        filterTypes,
     },
+    useFilters,
+    useGlobalFilter,
     usePagination
   );
 
@@ -108,7 +197,9 @@ export default function Table({ columns, data, hiddenColumns = [], onCloseModal,
             {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                <th {...column.getHeaderProps()}>{column.render("Header")}
+                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
                 ))}
             </tr>
             ))}
