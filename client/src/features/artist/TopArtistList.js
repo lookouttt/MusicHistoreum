d839a069219bd9638b274bb060aaa5de696071ea
lookouttt <lookoutt@hotmail.com>
2023-02-15 22:09:18 -0800

    Changing top artist list to work on A2 server.

--- client/src/features/artist/TopArtistList.js
+++ client/src/features/artist/TopArtistList.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 import { getTopArtists } from "./artistsSlice";
 
 const TopArtistList = () => {
+    const navigate = useNavigate();
     const artistList = useSelector(getTopArtists());
 
     return (artistList.map((artist, index) => {
-        return <li key={index} className="artistListName"><a href={`/Artist/${artist.ArtistName}`}>{artist.DisplayName}</a></li>
+        return (
+            <li>
+            <Link to={`/Artist/${artist.ArtistName}`} 
+                key={index}
+                className="artistListName"
+                onClick={(e) => {
+                    e.preventDefault();
+                    navigate(`/Artist/${artist.ArtistName}`);
+                }}>{artist.DisplayName}</Link>
+                </li>)
     }));
+    // return <li key={index} className="artistListName"><a href={`/Artist/${artist.ArtistName}`}>{artist.DisplayName}</a></li>
+
 }
 
 export default TopArtistList;
